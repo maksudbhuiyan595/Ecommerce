df08224ca101ddb8436c895d2eb252b225b05158
Maksud Bhuiyan <maksudbhuiyan595>
2023-11-30 01:34:04 +0600

    migrations all

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->string('role');
+            $table->foreignId('role_id')->constrained('roles')->restrictOnDelete();
+            
+            $table->string('token');
+            $table->string('token_expried_at');
+            
+            $table->string('otp');
+            $table->string('otp_expired_at');
+            $table->timestamp('email_verified_at')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
