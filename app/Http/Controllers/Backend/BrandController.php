57b2f6ed01a4833219dcd15e8827e8f8db3c64da
statementloop <rimizoo468@gmail.com>
2023-12-02 21:15:27 +0600

    login

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -24,42 +24,42 @@ class BrandController extends Controller
         return view('backend.pages.brands.create',compact('category'));
     }
 
-    public function store(Request $request)
-    {
-        $validate=Validator::make($request->all(),[
-            "brand_name"  =>"required |unique:brands,brand_name,string,id",
-            "image"  =>'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            "category_id"  =>"required"
-        ]);
-        if($validate->fails())
-        {
+    // public function store(Request $request)
+    // {
+    //     $validate=Validator::make($request->all(),[
+    //         "brand_name"  =>"required |unique:brands,brand_name,string,id",
+    //         "image"  =>'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    //         "category_id"  =>"required"
+    //     ]);
+    //     if($validate->fails())
+    //     {
             
-            notify()->error($validate->getMessageBag()->first());
-            return redirect()->back();
-        }
+    //         notify()->error($validate->getMessageBag()->first());
+    //         return redirect()->back();
+    //     }
 
-        $image = $request->file('image');
-        $imageName = time().'.'.$image->extension();
+    //     $image = $request->file('image');
+    //     $imageName = time().'.'.$image->extension();
        
-        $destinationPathThumbnail = public_path('/thumbnail');
-        $img = Image::make($image->path());
-        $img->resize(300, 300, function ($constraint) {
-            $constraint->aspectRatio();
-        })->save($destinationPathThumbnail.'/'.$imageName);
+    //     $destinationPathThumbnail = public_path('/thumbnail');
+    //     $img = Image::make($image->path());
+    //     $img->resize(300, 300, function ($constraint) {
+    //         $constraint->aspectRatio();
+    //     })->save($destinationPathThumbnail.'/'.$imageName);
      
-        $destinationPath = public_path('/images');
-        $image->move($destinationPath, $imageName);
+    //     $destinationPath = public_path('/images');
+    //     $image->move($destinationPath, $imageName);
 
-       Brand::create([
-        "brand_name"  =>$request->brand_name,
-        "brand_image"  =>$imageName,
-        "category_id"  =>$request->category_id,
+    //    Brand::create([
+    //     "brand_name"  =>$request->brand_name,
+    //     "brand_image"  =>$imageName,
+    //     "category_id"  =>$request->category_id,
        
         
-       ]);
+    //    ]);
        
-       notify()->success("successfully created brand.");
-       return redirect()->route("brand.list");
+    //    notify()->success("successfully created brand.");
+    //    return redirect()->route("brand.list");
 
-    }
+    // }
 }
